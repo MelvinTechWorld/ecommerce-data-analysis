149abd63f7a11bf73258c3445b1613d28a17c7de
Melvin Abraham <melvintechworld@gmail.com>
2026-02-08 12:25:56 -0800

    Add project README

--- Week_4_Loops/greetings.py
+++
@@ -1,9 +0,0 @@
-
-name = input("Enter your name: ")
-times = input("How many greetings do you want? ")
-
-if times.isnumeric():
-    for i in range(int(times)):
-        print(f"Hello {name}")
-else:
-    print("Please enter a valid number")
--- Week_4_Python_Basics/input_validation.py
+++
@@ -1,4 +0,0 @@
-
-# Validation on data received from the input() function
-
-age = input("Enter your age: ")
